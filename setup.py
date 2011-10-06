61a1456da2d7a6b74c1152432cae46346ef5a38c
morgan craft <mgan59@gmail.com>
2011-10-06 14:36:49 -0700

    minor errors in syntax packaging

--- setup.py
+++ setup.py
@@ -4,10 +4,18 @@ from distutils.core import setup
 
 from eqentia_client.client import __version__
 
-setup(name = "eqentia_client",
+setup(name = "python-eqentia",
       version = __version__,
       description = "A small library that provides an interface wrapper to Eqentia Rest Api",
       author = "Morgan Craft <http://www.morgancraft.com>",
       author_email = 'mgan59@gmail.com',
       license = 'MIT',
-      packages = ['eqentia_client'])
+      packages = ['eqentia_client'],
+      classifiers=['Development Status :: 4 - Beta',
+                         'Environment :: Web Environment',
+                         'Intended Audience :: Developers',
+                         'License :: OSI Approved :: MIT License',
+                         'Operating System :: OS Independent',
+                         'Programming Language :: Python',
+                         'Topic :: Blogging'],
+            )
